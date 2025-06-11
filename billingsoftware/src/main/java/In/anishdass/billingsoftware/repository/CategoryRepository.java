f7ad95b65086e121d1bb27d55bc34453b96598af
anishdass <anishdassatoffice@gmail.com>
2025-06-11 17:18:04 +0100

    API to delete category in place

--- billingsoftware/src/main/java/In/anishdass/billingsoftware/repository/CategoryRepository.java
+++ billingsoftware/src/main/java/In/anishdass/billingsoftware/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package In.anishdass.billingsoftware.repository;
 import In.anishdass.billingsoftware.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    
+    Optional<CategoryEntity> findByCategoryId(String categoryId);
 }
