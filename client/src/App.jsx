cc4981ae3f57061fc57ddfe30ef83b6bb0319429
anishdass <anishdassatoffice@gmail.com>
2025-06-13 17:11:59 +0100

    category form of manage category completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Menubar from "./components/Menubar/Menubar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import ManageCategory from "./pages/manageCategory/ManageCategories";
+import ManageUsers from "./pages/manageUsers/ManageUsers";
+import ManageItems from "./pages/manageItems/ManageItems";
+import Explore from "./pages/explore/Explore";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Menubar />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/categories' element={<ManageCategory />} />
+        <Route path='/users' element={<ManageUsers />} />
+        <Route path='/items' element={<ManageItems />} />
+        <Route path='/explore' element={<Explore />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
