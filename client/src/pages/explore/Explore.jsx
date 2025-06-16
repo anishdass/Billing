9ac27213dd851cd98450e00b27f94debd69d8206
anishdass <anishdassatoffice@gmail.com>
2025-06-16 17:39:40 +0100

    Building explore page

--- client/src/pages/explore/Explore.jsx
+++ client/src/pages/explore/Explore.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import "./Explore.css";
 
 const Explore = () => {
   return (
     <div className='explore-container text-light'>
       <div className='left-column'>
-        <div className='first-row' style={{ overflowY: "auto" }}></div>
+        <div className='first-row' style={{ overflowY: "auto" }}>
+          Categories
+        </div>
         <hr className='horizontal-line' />
-        <div className='second-row' style={{ overflowY: "auto" }}></div>
+        <div className='second-row' style={{ overflowY: "auto" }}>
+          Items
+        </div>
       </div>
-      <div className='right-column'>
-        <div className="customer-form-container"></div>
+      <div className='right-column d-flex flex-column'>
+        <div className='customer-form-container' style={{ height: "15%" }}>
+          Customer Form 
+        </div>
+        <hr className='my-3 text-light' />
+        <div
+          className='cart-items-container'
+          style={{ height: "55%", overflowY: "auto" }}>
+          Cart Items
+        </div>
+        <div className='cart-summary-container' style={{ height: "30%" }}>
+          Cart Summary
+        </div>
       </div>
     </div>
   );
