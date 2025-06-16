e415067ad01a6fc9c6b284fa87e1e7aa94a21050
anishdass <anishdassatoffice@gmail.com>
2025-06-16 17:00:38 +0100

    manage items page completed

--- client/src/pages/explore/Explore.jsx
+++ client/src/pages/explore/Explore.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className='explore-container text-light'>
+      <div className='left-column'>
+        <div className='first-row' style={{ overflowY: "auto" }}></div>
+        <hr className='horizontal-line' />
+        <div className='second-row' style={{ overflowY: "auto" }}></div>
+      </div>
+      <div className='right-column'>
+        <div className="customer-form-container"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Explore;
--- client/src/pages/manageItems/ManageItems.jsx
+++ client/src/pages/manageItems/ManageItems.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./ManageItems.css";
+import ItemForm from "../../components/itemForm/ItemForm";
+import ItemList from "../../components/itemList/ItemList";
 
 const ManageItems = () => {
   return (
     <div className='items-container text-light'>
-      <div className='left-column'>Category form</div>
-      <div className='right-column'>List of categories</div>
+      <div className='left-column'>
+        <ItemForm />
+      </div>
+      <div className='right-column'>
+        <ItemList />
+      </div>
     </div>
   );
 };
